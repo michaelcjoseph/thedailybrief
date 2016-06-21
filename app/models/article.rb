659ac1645515944e9dcdc399be3ef286c0da6f17
MichaelCJoseph <mjoseph.cm@gmail.com>
2016-06-20 22:04:40 -0700

    Add uniqueness constraints to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ActiveRecord::Base
 	validates :source, presence: true
 	validates :date, presence: true
-	validates :web_url, presence: true
-	validates :title, presence: true
-	validates :snippet, presence: true
+	validates :web_url, presence: true, uniqueness: true
+	validates :title, presence: true, uniqueness: true
+	validates :snippet, presence: true, uniqueness: true
 	validates :word_count, presence: true
 	validates :views, presence: true
 end
